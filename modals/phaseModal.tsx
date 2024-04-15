8e0b22af2c6a4a239913a298720a1217698f9824
kowalski <leejhwalker@gmail.com>
2024-04-15 17:34:04 +0200

    updated the view orders modal to have similar style to the schedule screen

--- modals/phaseModal.tsx
+++ modals/phaseModal.tsx
@@ -1,8 +1,9 @@
 // phaseModal.tsx
 import React, { useEffect, useState } from 'react';
-import { View, Text, Modal, TouchableOpacity, ScrollView } from 'react-native';
+import { View, Text, Modal, TouchableOpacity, ScrollView, StyleSheet } from 'react-native';
 import { getOrderAssignments } from '../config/apiCalls';
 import * as Keychain from 'react-native-keychain';
+import fonts from '../config/fonts';
 
 interface PhaseModalProps {
     onClose: () => void;
@@ -39,7 +40,56 @@ const PhaseModal: React.FC<PhaseModalProps> = ({ onClose, orderNumber }) => {
         };
         fetchAssignments();
     }, [orderNumber]);
-    
+
+    const formatDate = (dateString: string | null) => {
+        if (dateString) {
+            const date = new Date(dateString);
+            const roundedMinutes = Math.round(date.getMinutes() + date.getSeconds() / 60);
+            const hours = date.getHours();
+            const minutes = roundedMinutes < 10 ? '0' + roundedMinutes : roundedMinutes;
+            const day = date.getDate() < 10 ? '0' + date.getDate() : date.getDate();
+            const month = (date.getMonth() + 1) < 10 ? '0' + (date.getMonth() + 1) : (date.getMonth() + 1);
+            const year = date.getFullYear();
+            return `${day}/${month}/${year} ${hours}:${minutes}`;
+        }
+        return '---';
+    };
+
+    const styles = StyleSheet.create({
+        container: {
+            flex: 1,
+            padding: 10,
+            backgroundColor: 'black',
+        },
+        tableHeader: {
+            flexDirection: 'row',
+            marginBottom: 10,
+            paddingHorizontal: 10,
+        },
+        headerCell: {
+            flex: 1,
+            fontWeight: 'bold',
+            textAlign: 'center',
+        },
+        assignmentItem: {
+            flexDirection: 'row',
+            justifyContent: 'space-between',
+            marginBottom: 10,
+            padding: 10,
+            backgroundColor: '#AD8457', // Gold color for assignments without staff_end
+        },
+        tableCell: {
+            flex: 1,
+            textAlign: 'center',
+        },
+        dismissedItem: {
+            backgroundColor: 'red',
+        },
+        closeButton: {
+            fontSize: 44,
+            color: 'black',
+        },
+    });
 
     return (
         <Modal
@@ -48,20 +98,25 @@ const PhaseModal: React.FC<PhaseModalProps> = ({ onClose, orderNumber }) => {
             visible={true}
             onRequestClose={onClose}
         >
-            <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: 'white' }}>
+            <View style={styles.container}>
                 <ScrollView>
-                    <Text style={{ color: 'black', fontSize: 20, marginBottom: 20 }}>Order Number: {orderNumber}</Text>
+                    <View style={styles.tableHeader}>
+                        <Text style={[fonts.txtList, styles.headerCell]}>Segment</Text>
+                        <Text style={[fonts.txtList, styles.headerCell]}>Resource</Text>
+                        <Text style={[fonts.txtList, styles.headerCell]}>Start</Text>
+                        <Text style={[fonts.txtList, styles.headerCell]}>End</Text>
+                    </View>
                     {assignments.map((assignment, index) => (
-                        <View key={index} style={{ marginVertical: 10 }}>
-                            <Text>Segment Name: {assignment.segment_name}</Text>
-                            <Text>Resource Item Code: {assignment.resource_item_code}</Text>
-                            <Text>Start Time: {assignment.staff_start || assignment.segment_start}</Text>
-                            <Text>End Time: {assignment.staff_end || assignment.segment_end}</Text>
+                        <View key={index} style={[styles.assignmentItem, assignment.staff_end ? styles.dismissedItem : {}]}>
+                            <Text style={[fonts.txtList, styles.tableCell]}>{assignment.segment_name}</Text>
+                            <Text style={[fonts.txtList, styles.tableCell]}>{assignment.resource_item_code}</Text>
+                            <Text style={[fonts.txtList, styles.tableCell]}>{formatDate(assignment.staff_start || assignment.segment_start)}</Text>
+                            <Text style={[fonts.txtList, styles.tableCell]}>{formatDate(assignment.staff_end || assignment.segment_end)}</Text>
                         </View>
                     ))}
                 </ScrollView>
                 <TouchableOpacity onPress={onClose}>
-                    <Text style={{ color: 'black', fontSize: 24 }}>Close</Text>
+                    <Text style={styles.closeButton}>Close</Text>
                 </TouchableOpacity>
             </View>
         </Modal>
